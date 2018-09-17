c25dea424d19f7c3df8193c933f506140aa2b6f7
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-09-17 18:25:37 -0500

    Button changes appearance on click and keeps the state

--- src/components/holdButton/holdButton.js
+++ src/components/holdButton/holdButton.js
@@ -1,17 +1,34 @@
 const holdButtonFactory = (cssClass) =>{
     let _htmlButton = document.createElement('button');
     let _cssClass = cssClass;
+    let _defaultCssClass = "btn-secondary";
     const _init = (width, height)=>{
-        ["btn", "btn-secondary"].forEach((cssClass) =>{
+        ["btn", _defaultCssClass].forEach((cssClass) =>{
             _htmlButton.classList.toggle(cssClass);
         });
         _htmlButton.style.width = `${width}px`;
         _htmlButton.style.height = `${height}px`;
+        _htmlButton.addEventListener('click', _onClick)
     };
     const setParent = (parentNode, childNode = _htmlButton ) =>{
         parentNode.appendChild(childNode);
     };
+    const _onClick = ()=>{
+        _toggleAppearance();
+        _togglePressed();
+    };
+    const _toggleAppearance = (cssClass = _cssClass, defaultCssClass = _defaultCssClass, pressed = _pressed)=>{
+        if(!pressed){
+            _htmlButton.classList.toggle(defaultCssClass);
+            _htmlButton.classList.toggle(cssClass)
+        }
+    };
+    const _togglePressed = ()=>{
+        _pressed = true;
+    };
+    let _pressed = false;
     _init(50, 50);
+
     return {setParent}
 };
 
