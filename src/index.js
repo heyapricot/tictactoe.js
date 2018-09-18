46b6ef93a3366474cab7d161f96229c171dc8b58
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-09-18 15:56:06 -0500

    Cells alternate between button class and symbol

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const {holdButtonFactory} = require('./components/holdButton/holdButton');
 const mainContainer = document.getElementById('mainContainer');
+let cells = [];
+const buttonClasses = [["btn-primary"], ["btn-warning"]];
+const icons = [["fas", "fa-times", "fa-2x"], ["far", "fa-circle", "fa-2x"]];
+let turn = 0;
+const onCellClick = (cell)=>{
+    cell.setIcon(icons[turn % 2]);
+    cell.setCssClass(buttonClasses[turn % 2]);
+    turn++;
+};
 const generateGrid = (()=>{
     let rowQuantity = 3;
     let columnQuantity = 3;
@@ -15,8 +24,14 @@ const generateGrid = (()=>{
             column.appendChild(buttonRowContainer);
             for(let j = 0; j < columnQuantity; j++){
                 let button = holdButtonFactory();
+                cells.push(button);
                 button.setParent(buttonRowContainer);
             }
         })();
     }
+})();
+const initCells = (()=>{
+    cells.forEach((cell)=>{
+        cell.addClickFunction(onCellClick.bind(this, cell));
+    });
 })();
